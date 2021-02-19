14fbfcc951fc5a0d1f600977bed627dc8876feb9
vladimir kutysh <webexpert0524@gmail.com>
2021-02-20 02:41:33 +0300

    eye

---
+++ app/precache-manifest.c071c2dbf23946c47f1e9d76e79fbc30.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d833fc9ea84f5bc25e8f16ef8d9ce07",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4557a5bda43f36cd02aa",
+    "url": "/static/css/2.5ca5743e.chunk.css"
+  },
+  {
+    "revision": "14ef0f57ed6cb9ccdaa9",
+    "url": "/static/css/main.beb46501.chunk.css"
+  },
+  {
+    "revision": "4557a5bda43f36cd02aa",
+    "url": "/static/js/2.45a789b8.chunk.js"
+  },
+  {
+    "revision": "14ef0f57ed6cb9ccdaa9",
+    "url": "/static/js/main.1df4dc67.chunk.js"
+  },
+  {
+    "revision": "92c89455b99cd109be4a",
+    "url": "/static/js/runtime-main.b5d1efea.js"
+  }
+]);
